1e252e374ddd89265898187966a1d678b72e3200
Marcella Thamrin <marcella.t23@mhs.istts.ac.id>
2025-05-13 17:53:19 +0700

    halaman login

--- app/src/main/java/com/example/nutrisaver/ui/theme/Type.kt
+++ app/src/main/java/com/example/nutrisaver/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.nutrisaver.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.nutrisaver.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,12 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val OpenSans = FontFamily(
+    Font(R.font.open_sans_regular),
+    Font(R.font.open_sans_bold, FontWeight.Bold),
+    Font(R.font.open_sans_semi_bold, FontWeight.SemiBold),
+    Font(R.font.open_sans_light, FontWeight.Light),
+    Font(R.font.open_sans_extra_bold, FontWeight.ExtraBold)
 )
