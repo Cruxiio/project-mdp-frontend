b2f97692df59b228621e4cf16dec9d5bf781e3ac
Marcella Thamrin <marcella.t23@mhs.istts.ac.id>
2025-05-14 18:09:30 +0700

    tampilan login register jadi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth")
+
+    implementation("androidx.navigation:navigation-compose:2.9.0")
 }
--- app/src/main/java/com/example/nutrisaver/MainActivity.kt
+++ app/src/main/java/com/example/nutrisaver/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.nutrisaver
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,7 +20,6 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
@@ -31,9 +28,9 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -48,9 +45,6 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -59,6 +53,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.nutrisaver.ui.theme.NutriSaverTheme
 import com.example.nutrisaver.ui.theme.OpenSans
 
@@ -68,8 +65,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NutriSaverTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginScreen(innerPadding)
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "login") {
+                    composable("login") {
+                        LoginScreen(onNavigateToRegister = {
+                            navController.navigate("register")
+                        })
+                    }
+                    composable("register") {
+                        RegisterScreen(onNavigateToLogin = {
+                            navController.navigate("login")
+                        })
+                    }
                 }
             }
         }
@@ -77,7 +85,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LoginScreen(innerPadding: PaddingValues = PaddingValues()) {
+fun LoginScreen(onNavigateToRegister: () -> Unit) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var rememberMe by remember { mutableStateOf(false) }
@@ -86,8 +94,7 @@ fun LoginScreen(innerPadding: PaddingValues = PaddingValues()) {
     val greenTealDark = colorResource(id = R.color.green_teal_dark)
     val greenGradient = Brush.horizontalGradient(listOf(green, greenTealDark))
 
-    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
-    val circleSize = screenWidth
+    val circleSize = LocalConfiguration.current.screenWidthDp.dp
 
     Box(modifier = Modifier
         .fillMaxSize(),
@@ -119,9 +126,12 @@ fun LoginScreen(innerPadding: PaddingValues = PaddingValues()) {
                 .zIndex(1f),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.height(20.dp))
+            Spacer(modifier = Modifier.height(40.dp))
             Image(
-                painter = painterResource(R.drawable.ic_launcher_foreground),
+                painter = painterResource(R.drawable.logo_white),
+                modifier = Modifier
+                    .size(90.dp)
+                    .padding(bottom = 10.dp),
                 contentDescription = "App icon"
             )
             Text(
@@ -150,25 +160,36 @@ fun LoginScreen(innerPadding: PaddingValues = PaddingValues()) {
                     .padding(horizontal = 32.dp),
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
-                Text("Username", fontSize = 18.sp, fontWeight = FontWeight.Bold)
-                OutlinedTextField(
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    value = username,
-                    onValueChange = { username = it },
+                        .fillMaxWidth()
+                ) {
+                    Text("Username", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(10.dp))
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        value = username,
+                        onValueChange = { username = it },
 
-                )
+                        )
+                }
                 Spacer(modifier = Modifier.height(10.dp))
-                Text("Password", fontSize = 18.sp, fontWeight = FontWeight.Bold)
-                OutlinedTextField(
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    value = password,
-                    onValueChange = { password = it },
-                    label = {  },
-                    visualTransformation = PasswordVisualTransformation(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-                )
+                        .fillMaxWidth()
+                ) {
+                    Text("Password", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        value = password,
+                        onValueChange = { password = it },
+                        label = {  },
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                    )
+                }
                 Spacer(modifier = Modifier.height(10.dp))
                 Row(verticalAlignment = Alignment.CenterVertically,) {
                     Checkbox(
@@ -233,7 +254,7 @@ fun LoginScreen(innerPadding: PaddingValues = PaddingValues()) {
                     Spacer(modifier = Modifier.height(10.dp))
                     TextButton(
                         onClick = {
-                            // LOGIKA KE HALAMAN REGISTER
+                            onNavigateToRegister()
                         }
                     ) {
                         Text("Create an Account",
@@ -245,17 +266,216 @@ fun LoginScreen(innerPadding: PaddingValues = PaddingValues()) {
                                 color = colorResource(R.color.green_dark)
                             ))
                     }
-                    Spacer(modifier = Modifier.height(30.dp))
+                    Spacer(modifier = Modifier.height(50.dp))
                 }
             }
         }
     }
 }
 
+@Composable
+fun RegisterScreen(onNavigateToLogin: () -> Unit) {
+    var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+
+    val yellow_lemon = colorResource(id = R.color.yellow_lemon)
+    val green = colorResource(id = R.color.green)
+    val greenTealDark = colorResource(id = R.color.green_teal_dark)
+    val greenGradient = Brush.horizontalGradient(listOf(green, greenTealDark))
+    val greenDark = colorResource(id = R.color.green_dark)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 32.dp),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Spacer(modifier = Modifier.height(30.dp))
+            Text("Create Account",
+                style = TextStyle(
+                    fontSize = 32.sp,
+                    fontFamily = OpenSans,
+                    fontWeight = FontWeight.Bold,
+                    color = green
+                )
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(greenGradient)
+                    .padding(all = 15.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text("Account Information",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontFamily = OpenSans,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    )
+                    HintBasicTextField(
+                        value = username,
+                        onValueChange = { username = it },
+                        hint = "Username",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                    )
+                    HintBasicTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        hint = "Email",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                    )
+                    HintBasicTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        hint = "Password",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                    )
+                    HintBasicTextField(
+                        value = confirmPassword,
+                        onValueChange = { confirmPassword = it },
+                        hint = "Confirm Password",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(30.dp))
+                    Button(
+                        onClick = {
+                            // LOGIKA REGISTER DISINI
+                        },
+                        contentPadding = PaddingValues(),
+                        shape = CircleShape,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = yellow_lemon
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    ) {
+                        Text(
+                            text = "Register",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontFamily = OpenSans,
+                                fontWeight = FontWeight.Bold,
+                                color = greenDark
+                            )
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(60.dp))
+            HorizontalDivider(
+                thickness = 2.dp
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Already Have an Account?",
+                    fontSize = 20.sp,
+                    fontFamily = OpenSans
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                TextButton(
+                    onClick = {
+                        onNavigateToLogin()
+                    }
+                ) {
+                    Text("Sign In",
+                        fontSize = 20.sp,
+                        style = TextStyle(
+                            textDecoration = TextDecoration.Underline,
+                            fontFamily = OpenSans,
+                            fontWeight = FontWeight.Bold,
+                            color = colorResource(R.color.green_dark)
+                        ))
+                }
+                Spacer(modifier = Modifier.height(50.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun HintBasicTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    hint: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .background(Color.White, RoundedCornerShape(10.dp))
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            singleLine = true,
+            textStyle = TextStyle(
+                color = Color.Black,
+                fontSize = 16.sp
+            ),
+            modifier = Modifier.fillMaxWidth(),
+            decorationBox = { innerTextField ->
+                if (value.isEmpty()) {
+                    Text(
+                        text = hint,
+                        color = Color.Gray,
+                        fontSize = 16.sp
+                    )
+                }
+                innerTextField()
+            }
+        )
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
-fun LoginScreenPreview() {
+fun Preview() {
     NutriSaverTheme {
-        LoginScreen()
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "login") {
+            composable("login") {
+                LoginScreen(onNavigateToRegister = {
+                    navController.navigate("register")
+                })
+            }
+            composable("register") {
+                RegisterScreen(onNavigateToLogin = {
+                    navController.navigate("login")
+                })
+            }
+        }
     }
 }
