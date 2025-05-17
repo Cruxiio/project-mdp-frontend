365d7b882a1af554b2664dbc7c721a8e3a84b708
Marcella Thamrin <marcella.t23@mhs.istts.ac.id>
2025-05-17 11:41:36 +0700

    screens dipisah

--- app/src/main/java/com/example/nutrisaver/MyAppNavigation.kt
+++ app/src/main/java/com/example/nutrisaver/MyAppNavigation.kt
@@ -2,14 +2,20 @@ package com.example.nutrisaver
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.nutrisaver.ui.screens.HomeScreen
+import com.example.nutrisaver.ui.screens.LoginScreen
+import com.example.nutrisaver.ui.screens.RegisterScreen
 
 @Composable
-fun MyAppNavigation (modifier : Modifier  = Modifier, authViewModel: AuthViewModel) {
-    val navController = rememberNavController()
-
+fun MyAppNavigation(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    authViewModel: AuthViewModel
+) {
     NavHost(navController = navController, startDestination = "Login") {
         composable("Login") {
             LoginScreen(modifier, navController, authViewModel)
